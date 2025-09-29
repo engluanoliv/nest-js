6d00c0ab59f5dd803855afbdb8a3c6ec3d0fe45a
engluanoliv <engluanoliv@gmail.com>
2025-09-29 13:17:42 -0300

    feat: adjusted the services, controllers and modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
-import { PrismaService } from './prisma/prisma.service';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [AuthModule, UserModule, PrismaModule],
-  controllers: [],
-  providers: [PrismaService],
 })
 export class AppModule {}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from "@nestjs/common";
+import { Inject, Injectable } from '@nestjs/common';
+import { Prisma, User } from 'generated/prisma';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  @Inject()
+  private readonly prisma: PrismaService;
+
+  async createUser(data: Prisma.UserCreateInput) {
+    return this.prisma.user.create({ data });
+  }
+
+  async updateUser(params: {
+    where: Prisma.UserWhereUniqueInput;
+    data: Prisma.UserUpdateInput;
+  }): Promise<User> {
+    const { where, data } = params;
+    return this.prisma.user.update({
+      data,
+      where,
+    });
+  }
+
+  async deleteUser(where: Prisma.UserWhereUniqueInput): Promise<User> {
+    return this.prisma.user.delete({
+      where,
+    });
+  }
+}
